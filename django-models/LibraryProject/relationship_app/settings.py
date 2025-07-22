cca39caeac63126343e981602159679dea932259
Adewale <waliyullahadewale30@gmail.com>
2025-07-22 12:49:39 +0100

    Add user authentication views and templates (login, logout, register)

--- django-models/LibraryProject/relationship_app/settings.py
+++ django-models/LibraryProject/relationship_app/settings.py
@@ -1,4 +1,6 @@
 INSTALLED_APPS = [
     ...
     'relationship_app',
+    LOGIN_REDIRECT_URL = '/'
+    LOGOUT_REDIRECT_URL = '/login/'
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Library  # ✅ This line must be here exactly
+from .models import Library #This line must be here exactly
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth import login
 
 # Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
 # Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+
+class RegisterView(FormView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('login')
