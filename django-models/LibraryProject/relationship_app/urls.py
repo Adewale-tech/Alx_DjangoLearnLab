0351c1fcdad58be404eca4061ad8316647963e4e
Adewale <waliyullahadewale30@gmail.com>
2025-07-23 13:05:50 +0100

    correct updated files in indentetion of class based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
 
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
+from . import Views
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
