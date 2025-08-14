3296939f811a93c55a30769e3f645ab1c220ff17
Adewale <waliyullahadewale30@gmail.com>
2025-08-14 12:30:13 +0100

    DELIVERED PROJECT STRUCTURE, CODE FILE , STATIC AND TEMPLATE DIRECTORY

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS' : [BASE_DIR / 'templates'],
     },
 ]
 
@@ -115,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
