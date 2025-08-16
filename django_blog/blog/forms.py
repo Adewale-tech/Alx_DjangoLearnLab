baf45377a779a718a5f7dfd97e86ed77570f1cc4
Adewale <waliyullahadewale30@gmail.com>
2025-08-16 15:17:28 +0100

    Creating blog post management features.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Post
 
 class CustomUserCreationForm(UserCreationForm):
     """
@@ -27,4 +27,15 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio']
-        # Add 'profile_picture' if using image field
+
+class PostForm(forms.ModelForm):
+    """
+    Form for creating and updating Post model instances.
+    """
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter post title'}),
+            'content': forms.Textarea(attrs={'placeholder': 'Enter post content'}),
+        }
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm, ProfileForm
-from .models import Profile
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import CustomUserCreationForm, ProfileForm, PostForm
+from .models import Profile, Post
 
 def index(request):
     """
@@ -20,7 +22,7 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            Profile.objects.create(user=user)  # Create Profile for new user
+            Profile.objects.create(user=user)
             login(request, user)
             return redirect('profile')
         else:
@@ -32,7 +34,7 @@ def register(request):
 @login_required
 def profile(request):
     """
-    View to display and update user profile, including User and Profile models.
+    View to display and update user profile.
     """
     profile, created = Profile.objects.get_or_create(user=request.user)
     if request.method == 'POST':
@@ -47,5 +49,57 @@ def profile(request):
         profile_form = ProfileForm(instance=profile)
     return render(request, 'blog/profile.html', {
         'user_form': user_form,
-        'profile_form': profile_form,
+        'profile_form': profile_form
     })
+
+class PostListView(ListView):
+    """
+    View to list all blog posts.
+    """
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    """
+    View to display a single blog post.
+    """
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """
+    View to create a new blog post, restricted to authenticated users.
+    """
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    View to update a blog post, restricted to the post's author.
+    """
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    View to delete a blog post, restricted to the post's author.
+    """
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
