8dfe872c2f560206fc0df04a0d81b98120a771e0
waliyullah <waliyullahadewale30@gamil.com>
2025-08-21 12:56:35 +0100

    Add custom user model and update settings for authentication

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class CustomUser(AbstractUser):
+    """
+    Custom user model extending AbstractUser with additional fields.
+    """
+    bio = models.TextField(blank=True, help_text="User biography")
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True, help_text="User profile picture")
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True,
+        help_text="Users following this user"
+    )
+
+    def __str__(self):
+        return self.username
 # Create your models here.
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
