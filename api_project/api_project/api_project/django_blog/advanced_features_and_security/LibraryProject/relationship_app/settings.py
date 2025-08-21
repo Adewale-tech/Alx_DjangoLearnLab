9fb2a103e4056b06472bb686313f41604a694a5d
waliyullah <waliyullahadewale30@gamil.com>
2025-08-21 12:00:01 +0100

    remove adirectory

--- api_project/api_project/api_project/django_blog/advanced_features_and_security/LibraryProject/relationship_app/settings.py
+++
@@ -1,9 +0,0 @@
-INSTALLED_APPS = [
-    # ...other installed apps...
-    'relationship_app',
-]
-
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
-
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
--- api_project/django_blog/django_blog/Introduction_to_Django/LibraryProject/bookshelf/delete.md
+++
@@ -1,9 +0,0 @@
-from bookshelf.models import Book
-
-# Delete the book
-book = Book.objects.get(title="Nineteen Eighty-Four")
-book.delete()
-
-# Confirm it's deleted
-Book.objects.all()
-
